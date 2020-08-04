4afe9460b7720d284154f8c88d75824bbbde488d
tilak <tilak.panchal@pasarpolis.com>
2020-08-04 20:28:26 +0530

    update the path

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'media';
 // Incrementing OFFLINE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 // const OFFLINE_VERSION = 1;
-const OFFLINE_URL = '/media/index.html';
+const INDEX_URL = '/media/index.html';
 const KEY_MAP = {
   interaction: 'event',
   client: 'customer',
@@ -18,7 +18,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        '/index.html'
+        INDEX_URL
       ]);
     })
   );

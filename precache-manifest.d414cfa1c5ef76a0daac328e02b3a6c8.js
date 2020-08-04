8964039fcac987dc392f3271d2b8ea24363b2838
tilak <tilak.panchal@pasarpolis.com>
2020-08-04 20:53:26 +0530

    Updates

--- precache-manifest.d414cfa1c5ef76a0daac328e02b3a6c8.js
+++ precache-manifest.d414cfa1c5ef76a0daac328e02b3a6c8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "303a6fd52ce3e72db53a7d3d5aa2ffe6",
+    "revision": "f96b7a62de27f399520257db624fe35f",
     "url": "/media/index.html"
   },
   {
-    "revision": "5fa0c52034ce2ebee698",
-    "url": "/media/static/css/main.5f361e03.chunk.css"
+    "revision": "4a59844738af36090628",
+    "url": "/media/static/css/main.9dcf2aac.chunk.css"
   },
   {
     "revision": "e272f20c8c9eedd907de",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/media/static/js/2.2cf3b860.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5fa0c52034ce2ebee698",
-    "url": "/media/static/js/main.211da2f5.chunk.js"
+    "revision": "4a59844738af36090628",
+    "url": "/media/static/js/main.426a2746.chunk.js"
   },
   {
     "revision": "8c0a72e04b15c4e6a9b1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/media/precache-manifest.6a04cf7a5f314064ebf80ccfef8d41bf.js"
+  "/media/precache-manifest.d414cfa1c5ef76a0daac328e02b3a6c8.js"
 );
 
 self.addEventListener('message', (event) => {

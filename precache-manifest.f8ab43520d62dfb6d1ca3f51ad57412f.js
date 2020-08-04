c9c9855049328b84cd24839d0efad9f32a4fe51e
tilak <tilak.panchal@pasarpolis.com>
2020-08-04 20:10:58 +0530

    Updates

---
+++ precache-manifest.f8ab43520d62dfb6d1ca3f51ad57412f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76d5a6bda0c2698b6a15902f61559b3",
+    "url": "/media/index.html"
+  },
+  {
+    "revision": "ee31520eac7ffd62eaab",
+    "url": "/media/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e272f20c8c9eedd907de",
+    "url": "/media/static/js/2.2cf3b860.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/media/static/js/2.2cf3b860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee31520eac7ffd62eaab",
+    "url": "/media/static/js/main.22c74c72.chunk.js"
+  },
+  {
+    "revision": "8c0a72e04b15c4e6a9b1",
+    "url": "/media/static/js/runtime-main.e0e5038d.js"
+  }
+]);
